b123eabce7c1691d7c67739b679125b6045a32e3
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-06-30 13:33:57 -0500

    Added password entry. Finished button layout on home screen. Gave buttons press in effect.

--- Capture Avatars Unity/Assets/Scripts/ButtonGroup.cs
+++ Capture Avatars Unity/Assets/Scripts/ButtonGroup.cs
@@ -2,18 +2,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+using DG.Tweening;
 
-public class ButtonGroup : MonoBehaviour
+public class ButtonGroup : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image _circle;
+    [SerializeField] private Image _shadow;
+    [SerializeField] private Image _icon;
+    [SerializeField] private TextMeshProUGUI _label;
+
+    [SerializeField] private Color _accentColor;
+    [SerializeField] private Color _textHighlightColor;
+
+    [SerializeField] private float _pressedInScale;
+    [SerializeField] private float _secondsToPressIn;
+    [SerializeField] private float _secondsToUnpress;
+
+    private Vector3 _initialShadowPosition;
+
+    void Awake()
     {
-        
+        _initialShadowPosition = _shadow.transform.localPosition;
+
+        if (_icon == null)
+        {
+            _textHighlightColor = Color.white;
+        }
+        else
+        {
+            _textHighlightColor = _accentColor;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        _circle.color = _accentColor;
+        _label.color = _textHighlightColor;
+        if (_icon != null)
+        {
+            _icon.color = Color.white;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _circle.color = Color.white;
+        _label.color = Color.black;
+        if (_icon != null)
+        {
+            _icon.color = Color.black;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.DOScale(_pressedInScale, _secondsToPressIn);
+        _shadow.transform.DOLocalMove(Vector3.zero, _secondsToPressIn);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.DOScale(1, _secondsToUnpress);
+        _shadow.transform.DOLocalMove(_initialShadowPosition, _secondsToUnpress);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerClick");
     }
 }
---
+++ Capture Avatars Unity/Assets/Scripts/PasswordManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PasswordManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _inputField;
+    [SerializeField] private TextMeshProUGUI _hint;
+    private bool _hasStartedCoroutine;
+
+    private void Awake()
+    {
+        _hasStartedCoroutine = false;
+    }
+
+    private void OnEnable()
+    {
+        _inputField.onValueChanged.AddListener(CheckPassword);
+    }
+
+    private void OnDisable()
+    {
+        _inputField.onValueChanged.RemoveListener(CheckPassword);
+    }
+
+    void CheckPassword(string text)
+    {
+        if (_inputField.text == "MCTOnsite" &&  !_hasStartedCoroutine)
+        {
+            StartCoroutine(Welcome());
+            _hasStartedCoroutine = true;
+        }
+    }
+
+    private IEnumerator Welcome()
+    {
+        _hint.text = "Welcome Jon Montego";
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
